ca1247bc736291bc5ec21a03874c40d56a04d9ab
Avinaba Dalal <d97.avinaba@gmail.com>
2023-08-19 11:25:51 +0530

    Add feature to msg any unknown whatsapp number
    Signed-off-by: Avinaba Dalal <d97.avinaba@gmail.com>

---
+++ app/src/main/java/com/corphish/quicktools/activities/WUPActivity.kt
@@ -0,0 +1,44 @@
+package com.corphish.quicktools.activities
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.corphish.quicktools.data.Constants
+
+/**
+ * WUP (WhatsApp Unknown Phone number) activity handles messaging to
+ * unknown phone numbers in whatsapp without saving them as a contact by
+ * opening it in wa.me/<phone_number>.
+ */
+class WUPActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        handleIntent(intent)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (intent.hasExtra(Intent.EXTRA_PROCESS_TEXT)) {
+            val text = intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT)
+            val regex = Regex(Constants.PHONE_NUMBER_REGEX)
+            if (text != null && regex.matches(text)) {
+                openInWeb(phoneNumber = text)
+            }
+        }
+
+        finish()
+    }
+
+    private fun openInWeb(phoneNumber: CharSequence) {
+        val url = "https://wa.me/$phoneNumber"
+        val browserIntent = Intent(Intent.ACTION_VIEW)
+        browserIntent.data = Uri.parse(url)
+        startActivity(browserIntent)
+    }
+}
---
+++ app/src/main/java/com/corphish/quicktools/data/Constants.kt
@@ -0,0 +1,9 @@
+package com.corphish.quicktools.data
+
+/**
+ * Constants.
+ */
+object Constants {
+    // Phone number regular expression
+    const val PHONE_NUMBER_REGEX = "^\\s*(?:\\+?(\\d{1,3}))?[-. (]*(\\d{3})[-. )]*(\\d{3})[-. ]*(\\d{4})(?: *x(\\d+))?\\s*\$"
+}
