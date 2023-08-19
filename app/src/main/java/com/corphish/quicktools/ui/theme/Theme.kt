b1ccc329a823257eae4f09f65d94c17565f71f5c
Avinaba Dalal <d97.avinaba@gmail.com>
2023-08-19 13:22:56 +0530

    Add launcher icon and colors
    Signed-off-by: Avinaba Dalal <d97.avinaba@gmail.com>

--- app/src/main/java/com/corphish/quicktools/ui/theme/Theme.kt
+++ app/src/main/java/com/corphish/quicktools/ui/theme/Theme.kt
@@ -16,15 +16,15 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = BlueForeground,
+    secondary = BlueBackground,
+    tertiary = BlueForeground
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = BlueForeground,
+    secondary = BlueBackground,
+    tertiary = BlueForeground
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
