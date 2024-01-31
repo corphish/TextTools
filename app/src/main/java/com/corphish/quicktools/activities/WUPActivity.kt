887770b4a5f0a3af02f26b3ee90c72fde2cb7293
Avinaba Dalal <d97.avinaba@gmail.com>
2024-01-31 10:36:08 +0530

    WUPActivity: Remove special characters from phone number before processing
    Signed-off-by: Avinaba Dalal <d97.avinaba@gmail.com>

--- app/src/main/java/com/corphish/quicktools/activities/WUPActivity.kt
+++ app/src/main/java/com/corphish/quicktools/activities/WUPActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.corphish.quicktools.data.Constants
 
 /**
@@ -14,20 +15,31 @@ import com.corphish.quicktools.data.Constants
 class WUPActivity : NoUIActivity() {
     override fun handleIntent(intent: Intent) {
         if (intent.hasExtra(Intent.EXTRA_PROCESS_TEXT)) {
-            val text = intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT)
+            val text = intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT)?.toString() ?: ""
+            val modified = specialCharactersRemovedFrom(text)
             val regex = Regex(Constants.PHONE_NUMBER_REGEX)
-            if (text != null && regex.matches(text)) {
-                openInWeb(phoneNumber = text)
+            if (regex.matches(modified)) {
+                openInWeb(phoneNumber = modified)
             }
         }
 
         finish()
     }
 
-    private fun openInWeb(phoneNumber: CharSequence) {
+    private fun openInWeb(phoneNumber: String) {
         val url = "https://wa.me/$phoneNumber"
+        Log.d("WUP", "https://wa.me/${specialCharactersRemovedFrom(phoneNumber)}")
         val browserIntent = Intent(Intent.ACTION_VIEW)
         browserIntent.data = Uri.parse(url)
         startActivity(browserIntent)
+    }
+
+    private fun specialCharactersRemovedFrom(phoneNumber: String) : String {
+        var res = phoneNumber
+        for (char in Constants.PHONE_NUMBER_SPECIAL_CHARACTERS) {
+            res = res.replace(char, "")
+        }
+
+        return res
     }
 }
--- app/src/main/java/com/corphish/quicktools/data/Constants.kt
+++ app/src/main/java/com/corphish/quicktools/data/Constants.kt
@@ -6,4 +6,7 @@ package com.corphish.quicktools.data
 object Constants {
     // Phone number regular expression
     const val PHONE_NUMBER_REGEX = "^\\s*(?:\\+?(\\d{1,3}))?[-. (]*(\\d{3})[-. )]*(\\d{3})[-. ]*(\\d{4})(?: *x(\\d+))?\\s*\$"
+
+    // Remove these special characters from the phone number
+    val PHONE_NUMBER_SPECIAL_CHARACTERS = listOf(" ", "-", "(", ")")
 }
