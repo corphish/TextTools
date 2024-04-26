cc1ab10ca166ec17b6ad3f89ad74e4dd3657c094
Avinaba Dalal <d97.avinaba@gmail.com>
2024-04-26 18:59:48 +0530

    app: Implement options activity for single option method
    Signed-off-by: Avinaba Dalal <d97.avinaba@gmail.com>

--- app/src/singleOption/java/com/corphish/quicktools/activities/OptionsActivity.kt
+++ app/src/singleOption/java/com/corphish/quicktools/activities/OptionsActivity.kt
@@ -1,46 +1,70 @@
 package com.corphish.quicktools.activities
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
+import android.content.Intent
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.StringRes
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.res.stringResource
+import com.corphish.quicktools.R
+import com.corphish.quicktools.extensions.truncate
+import com.corphish.quicktools.ui.common.ListDialog
 import com.corphish.quicktools.ui.theme.QuickToolsTheme
 
-class OptionsActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            QuickToolsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting2("Android")
+class OptionsActivity : NoUIActivity() {
+
+    private val _options = listOf(
+        Option(R.string.whatsapp, WUPActivity::class.java),
+        Option(R.string.eval_title_small, EvalActivity::class.java),
+        Option(R.string.transform_long, TransformActivity::class.java),
+    )
+
+    private val router = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == RESULT_OK) {
+            setResult(RESULT_OK, it.data)
+        }
+
+        finish()
+    }
+
+    override fun handleIntent(intent: Intent): Boolean {
+        if (intent.hasExtra(Intent.EXTRA_PROCESS_TEXT)) {
+            val text = intent.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT).toString()
+            val readonly = intent.getBooleanExtra(Intent.EXTRA_PROCESS_TEXT_READONLY, false)
+            setContent {
+                QuickToolsTheme {
+                    val optionDialog = remember {
+                        mutableStateOf(true)
+                    }
+
+                    if (optionDialog.value) {
+                        ListDialog(
+                            title = stringResource(id = R.string.app_name),
+                            message = stringResource(id = R.string.app_single_op, text.truncate()),
+                            list = _options,
+                            stringResourceSelector = { it.optionResourceId },
+                            onItemSelected = {
+                                val routeIntent = Intent(this, _options[it].handlingClass)
+                                routeIntent.putExtra(Intent.EXTRA_PROCESS_TEXT, text)
+                                routeIntent.putExtra(Intent.EXTRA_PROCESS_TEXT_READONLY, readonly)
+                                router.launch(routeIntent)
+                                optionDialog.value = false
+                            }) {
+                            optionDialog.value = false
+                            finish()
+                        }
+                    }
                 }
             }
+            return false
         }
-    }
-}
 
-@Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+        return true
+    }
+
+    data class Option(
+        @StringRes val optionResourceId: Int,
+        val handlingClass: Class<out NoUIActivity>,
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview3() {
-    QuickToolsTheme {
-        Greeting2("Android")
-    }
 }
