7ee9ec68a8e77d815a53877f84f110b1dbc8894c
Avinaba Dalal <d97.avinaba@gmail.com>
2024-04-26 14:46:04 +0530

    ui: Move list dialog to a common package
    Signed-off-by: Avinaba Dalal <d97.avinaba@gmail.com>

---
+++ app/src/main/java/com/corphish/quicktools/ui/common/ListDialog.kt
@@ -0,0 +1,105 @@
+package com.corphish.quicktools.ui.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.KeyboardArrowLeft
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.corphish.quicktools.ui.theme.BrandFontFamily
+import com.corphish.quicktools.ui.theme.TypographyV2
+
+/**
+ * A list of selectable options shown inside a dialog.
+ */
+@Composable
+fun <T> ListDialog(
+    title: String,
+    message: String,
+    list: List<T>,
+    supportBack: Boolean = false,
+    onItemSelected: (T) -> Unit,
+    stringResourceSelector: (T) -> Int,
+    onBackPressed: () -> Unit = {},
+    onDismissRequest: () -> Unit
+) {
+    Dialog(onDismissRequest = { onDismissRequest() }) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            shape = RoundedCornerShape(16.dp),
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp),
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                ) {
+                    if (supportBack) {
+                        IconButton(
+                            onClick = { onBackPressed() },
+                            modifier = Modifier
+                                .clip(CircleShape)
+                                .size(48.dp)
+                                .background(MaterialTheme.colorScheme.primary)
+                        ) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Outlined.KeyboardArrowLeft,
+                                contentDescription = "",
+                                modifier = Modifier.size(32.dp),
+                                tint = MaterialTheme.colorScheme.onPrimary
+                            )
+                        }
+                    }
+
+                    Text(
+                        text = title,
+                        style = TypographyV2.headlineMedium,
+                        fontFamily = BrandFontFamily,
+                        fontWeight = FontWeight.W200,
+                        color = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier.padding(start = if (supportBack) 16.dp else 0.dp)
+                    )
+                }
+
+                Text(
+                    text = message,
+                    style = TypographyV2.bodyMedium,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+
+                for (item in list) {
+                    Button(
+                        onClick = {
+                            onItemSelected(item)
+                        },
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(text = stringResource(id = stringResourceSelector(item)), fontFamily = BrandFontFamily,)
+                    }
+                }
+            }
+        }
+    }
+}
