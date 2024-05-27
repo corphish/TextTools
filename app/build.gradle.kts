ad97174f592ba3893b886d7013f26b75766389c5
Avinaba Dalal <d97.avinaba@gmail.com>
2024-05-27 19:06:01 +0530

    gradle: Bump version
    Signed-off-by: Avinaba Dalal <d97.avinaba@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.corphish.quicktools"
         minSdk = 30
         targetSdk = 34
-        versionCode = 11
-        versionName = "1.1.0.1"
+        versionCode = 12
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
