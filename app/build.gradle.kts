d7b205d74322204bec5098d8833e8cccbe7b33f0
Avinaba Dalal <d97.avinaba@gmail.com>
2024-10-26 12:35:12 +0530

    gradle: Update libs
    Signed-off-by: Avinaba Dalal <d97.avinaba@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -97,8 +97,8 @@ dependencies {
     implementation("androidx.preference:preference-ktx:1.2.1")
 
     // Compose
-    implementation(platform("androidx.compose:compose-bom:2024.09.03"))
-    implementation("androidx.activity:activity-compose:1.9.2")
+    implementation(platform("androidx.compose:compose-bom:2024.10.00"))
+    implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -110,7 +110,7 @@ dependencies {
     implementation("net.objecthunter:exp4j:0.4.8")
 
     // Testing
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.09.03"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.10.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 
     // Compose debug
