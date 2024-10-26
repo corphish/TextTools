022b1e66875c099c71f666c72524bff124294851
Avinaba Dalal <d97.avinaba@gmail.com>
2024-10-26 13:05:41 +0530

    Revert "gradle: Use BOM version only for compose libs"
    This reverts commit 601e2507752ea6e0b36994eaba29ece84dca8709.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,14 +98,14 @@ dependencies {
 
     // Compose
     implementation(platform("androidx.compose:compose-bom:2024.10.00"))
-    implementation("androidx.activity:activity-compose")
+    implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-text-google-fonts")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose")
-    implementation ("androidx.constraintlayout:constraintlayout-compose")
+    implementation ("androidx.constraintlayout:constraintlayout-compose:1.0.1")
 
     // To evaluate mathematical expressions
     implementation("net.objecthunter:exp4j:0.4.8")
