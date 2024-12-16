b27fad70830fa1de373f7bfac3df5432b5ef37b0
Avinaba Dalal <d97.avinaba@gmail.com>
2024-12-16 19:16:12 +0530

    settings: Use custom top app bar
    Signed-off-by: Avinaba Dalal <d97.avinaba@gmail.com>

--- app/src/main/java/com/corphish/quicktools/activities/SettingsActivity.kt
+++ app/src/main/java/com/corphish/quicktools/activities/SettingsActivity.kt
@@ -5,20 +5,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.outlined.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
@@ -27,34 +23,36 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import com.corphish.quicktools.R
+import com.corphish.quicktools.ui.common.CustomTopAppBar
 import com.corphish.quicktools.ui.theme.BrandFontFamily
 import com.corphish.quicktools.ui.theme.QuickToolsTheme
 import com.corphish.quicktools.ui.theme.Typography
@@ -69,132 +67,115 @@ class SettingsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            QuickToolsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Settings(settingsViewModel)
-                }
-            }
+            QuickToolsTheme { Settings(settingsViewModel) }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Settings(settingsViewModel: SettingsViewModel) {
     val uriHandler = LocalUriHandler.current
     val activity = (LocalContext.current as? Activity)
     val versionName by settingsViewModel.appVersionName.collectAsState()
     val versionCode by settingsViewModel.appVersionCode.collectAsState()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
 
-    Column(
-        modifier = Modifier
-            .padding(16.dp)
-            .verticalScroll(rememberScrollState())
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CustomTopAppBar(
+                id = R.string.title_activity_settings,
+                onNavigationClick = { activity?.finish() },
+                scrollBehavior = scrollBehavior
+            )
+        }
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(bottom = 8.dp)
+        Column(
+            modifier = Modifier
+                .padding(
+                    start = it.calculateStartPadding(LayoutDirection.Ltr).plus(16.dp),
+                    end = it.calculateEndPadding(LayoutDirection.Ltr).plus(16.dp),
+                    top = it.calculateTopPadding().plus(16.dp),
+                    bottom = it.calculateBottomPadding().plus(16.dp)
+                )
+                .verticalScroll(rememberScrollState())
         ) {
-            IconButton(
-                onClick = { activity?.finish() },
-                modifier = Modifier
-                    .clip(CircleShape)
-                    .background(MaterialTheme.colorScheme.primary)
+            Text(
+                text = stringResource(id = R.string.messaging),
+                style = TypographyV2.labelSmall,
+                modifier = Modifier.padding(bottom = 4.dp),
+                fontFamily = BrandFontFamily,
+                color = MaterialTheme.colorScheme.primary
+            )
+
+            TextSettings(settingsViewModel)
+
+            Text(
+                text = stringResource(id = R.string.eval_title_small),
+                style = TypographyV2.labelSmall,
+                modifier = Modifier.padding(top = 24.dp, bottom = 4.dp),
+                fontFamily = BrandFontFamily,
+                color = MaterialTheme.colorScheme.primary
+            )
+
+            EvaluateSettings(settingsViewModel)
+
+            Text(
+                text = stringResource(id = R.string.app_info),
+                style = TypographyV2.labelSmall,
+                modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
+                fontFamily = BrandFontFamily,
+                color = MaterialTheme.colorScheme.primary
+            )
+
+            Text(
+                text = stringResource(
+                    id = R.string.app_version,
+                    versionName,
+                    versionCode
+                ),
+                style = Typography.bodyMedium
+            )
+
+            Button(
+                onClick = { uriHandler.openUri("https://github.com/corphish/TextTools/releases") },
+                modifier = Modifier.padding(top = 8.dp)
             ) {
-                Icon(
-                    imageVector = Icons.AutoMirrored.Outlined.KeyboardArrowLeft,
-                    contentDescription = "",
-                    modifier = Modifier.size(32.dp),
-                    tint = MaterialTheme.colorScheme.onPrimary
+                Icon(painterResource(id = R.drawable.ic_open_in_new), contentDescription = "")
+                Text(
+                    text = stringResource(id = R.string.releases),
+                    modifier = Modifier.padding(start = 16.dp),
+                    style = TypographyV2.labelMedium,
+                    fontWeight = FontWeight.W600
                 )
             }
 
             Text(
-                text = stringResource(id = R.string.title_activity_settings),
-                style = TypographyV2.headlineLarge,
-                color = MaterialTheme.colorScheme.primary,
-                fontFamily = BrandFontFamily,
-                modifier = Modifier.padding(start = 16.dp)
-            )
-        }
-
-        Text(
-            text = stringResource(id = R.string.messaging),
-            style = TypographyV2.labelSmall,
-            modifier = Modifier.padding(top = 16.dp, bottom = 4.dp),
-            fontFamily = BrandFontFamily,
-            color = MaterialTheme.colorScheme.primary
-        )
-
-        TextSettings(settingsViewModel)
-
-        Text(
-            text = stringResource(id = R.string.eval_title_small),
-            style = TypographyV2.labelSmall,
-            modifier = Modifier.padding(top = 24.dp, bottom = 4.dp),
-            fontFamily = BrandFontFamily,
-            color = MaterialTheme.colorScheme.primary
-        )
-
-        EvaluateSettings(settingsViewModel)
-
-        Text(
-            text = stringResource(id = R.string.app_info),
-            style = TypographyV2.labelSmall,
-            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
-            fontFamily = BrandFontFamily,
-            color = MaterialTheme.colorScheme.primary
-        )
-
-        Text(
-            text = stringResource(
-                id = R.string.app_version,
-                versionName,
-                versionCode
-            ),
-            style = Typography.bodyMedium
-        )
-
-        Button(
-            onClick = { uriHandler.openUri("https://github.com/corphish/TextTools/releases") },
-            modifier = Modifier.padding(top = 8.dp)
-        ) {
-            Icon(painterResource(id = R.drawable.ic_open_in_new), contentDescription = "")
-            Text(
-                text = stringResource(id = R.string.releases),
-                modifier = Modifier.padding(start = 16.dp),
-                style = TypographyV2.labelMedium,
-                fontWeight = FontWeight.W600
-            )
-        }
-
-        Text(
-            text = stringResource(id = R.string.donate),
-            style = TypographyV2.labelSmall,
-            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
-            fontFamily = BrandFontFamily,
-            color = MaterialTheme.colorScheme.primary
-        )
-
-        Text(
-            text = stringResource(id = R.string.donate_msg),
-            style = Typography.bodyMedium
-        )
-
-        Button(
-            onClick = { uriHandler.openUri("https://www.paypal.com/paypalme/corphish") },
-            modifier = Modifier.padding(top = 8.dp)
-        ) {
-            Icon(Icons.Default.ThumbUp, contentDescription = "")
-            Text(
                 text = stringResource(id = R.string.donate),
-                modifier = Modifier.padding(start = 16.dp),
-                style = TypographyV2.labelMedium,
-                fontWeight = FontWeight.W600
+                style = TypographyV2.labelSmall,
+                modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
+                fontFamily = BrandFontFamily,
+                color = MaterialTheme.colorScheme.primary
             )
+
+            Text(
+                text = stringResource(id = R.string.donate_msg),
+                style = Typography.bodyMedium
+            )
+
+            Button(
+                onClick = { uriHandler.openUri("https://www.paypal.com/paypalme/corphish") },
+                modifier = Modifier.padding(top = 8.dp)
+            ) {
+                Icon(Icons.Default.ThumbUp, contentDescription = "")
+                Text(
+                    text = stringResource(id = R.string.donate),
+                    modifier = Modifier.padding(start = 16.dp),
+                    style = TypographyV2.labelMedium,
+                    fontWeight = FontWeight.W600
+                )
+            }
         }
     }
 }
