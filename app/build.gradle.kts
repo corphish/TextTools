7c28877805f191f8dcdd3881abea7efbe8ad8b12
Avinaba Dalal <d97.avinaba@gmail.com>
2024-12-18 20:37:55 +0530

    gradle: Update compose BOM version
    Signed-off-by: Avinaba Dalal <d97.avinaba@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -102,7 +102,7 @@ dependencies {
     implementation("androidx.preference:preference-ktx:1.2.1")
 
     // Compose
-    implementation(platform("androidx.compose:compose-bom:2024.11.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -121,7 +121,7 @@ dependencies {
 
 
     // Testing
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.11.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.12.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 
     // Compose debug
