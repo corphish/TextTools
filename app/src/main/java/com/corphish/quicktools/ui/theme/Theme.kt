23596d98a67213ac1cb7fac6d11c5dba2e592d55
Avinaba Dalal <d97.avinaba@gmail.com>
2024-12-28 11:23:58 +0530

    theme: Do not use colored statusbar
    In Android 15, window.statusBarColor is deprecated and does not work. As of now, there is no concrete solution for this.
    Hence, use the statusbar color defined by the system theme.
    Make sure the elements on the statusbar are properly visible.
    
    Signed-off-by: Avinaba Dalal <d97.avinaba@gmail.com>

--- app/src/main/java/com/corphish/quicktools/ui/theme/Theme.kt
+++ app/src/main/java/com/corphish/quicktools/ui/theme/Theme.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -54,11 +53,11 @@ fun QuickToolsTheme(
         else -> LightColorScheme
     }
     val view = LocalView.current
+
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
