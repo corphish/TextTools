6af3927cab349c85303ec4c90816c3a7f6865a6e
corphish <d97.avinaba@gmail.com>
2025-03-10 12:06:40 +0530

    gradle: Update libs
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,15 +79,15 @@ dependencies {
     implementation("androidx.preference:preference-ktx:1.2.1")
 
     // Compose
-    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
-    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation(platform("androidx.compose:compose-bom:2025.02.00"))
+    implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui-text-google-fonts")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose")
-    implementation ("androidx.constraintlayout:constraintlayout-compose:1.1.0")
+    implementation ("androidx.constraintlayout:constraintlayout-compose:1.1.1")
 
     // To evaluate mathematical expressions
     implementation("net.objecthunter:exp4j:0.4.8")
@@ -98,7 +98,7 @@ dependencies {
 
 
     // Testing
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.12.01"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2025.02.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 
     // Compose debug
