030607702b0066f78115506fd677c7c0a157d027
corphish <d97.avinaba@gmail.com>
2025-03-10 12:18:50 +0530

    gradle: Bump version
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.corphish.quicktools"
         minSdk = 30
         targetSdk = 35
-        versionCode = 19
-        versionName = "2.0.2"
+        versionCode = 20
+        versionName = "2.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
