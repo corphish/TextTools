378cb344e1d648705f4d332807fab845bd5683ce
corphish <d97.avinaba@gmail.com>
2025-03-10 12:05:17 +0530

    gradle: Update AGP
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.7.3" apply false
+    id("com.android.application") version "8.8.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
